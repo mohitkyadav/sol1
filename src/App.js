ba581814c13893c93df6ab2dcbc3f11b6bffb360
Mohit Kumar Yadav <mohitkyadav@outlook.com>
2022-08-20 19:18:30 +0200

    Add nick solution

--- src/App.js
+++ src/App.js
@@ -1,65 +1,148 @@
 import React from "react";
 import './App.css'
-// MD_PARSER
 
-const MD_TO_TOKEN_MAPPER = [
-  { pattern: /^# (.*$)/, tag: 'h1' },
-  { pattern: /^## (.*$)/, tag: 'h2' },
-  { pattern: /```(.*)```/, tag: 'code' },
-  { pattern: /(.*$)/, tag: 'p' }
-];
+// You have following object:
+const res = [
+  { line: 0, tag: "h1", text: "My name is Mohit." },
+  { line: 1, tag: "h2", text: "My name is actually not Mohit." },
+  {
+    line: 2,
+    tag: "p",
+    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."
+  },
+  { line: 3, tag: "code", text: "undefined < undefined // false" },
+]
 
-const parseMd = (md) => {
-  const mdLines = md.split('\n');
+// 1. On the page as a user I see one big text area and one preview pane.
+// 2. I should see the above response parsed as raw Markdown in the text area. Example: For first line in the text I should see:  # My name is Mohit.
+// 3. In the preview I should be able to see the rendered Markdown.
+// 4. As I change the text, the preview should also update.
+// Notes: You are not allowed to use any library to render or process markdown. Our app only supports h1, h2, p, and code, don’t worry about other tags.
 
-  return mdLines.map((line, idx) => {
-    const token = MD_TO_TOKEN_MAPPER.find(({ pattern }) => pattern.test(line));
-    return {
-      line: idx,
-      tag: token.tag,
-      text: line.replace(token.pattern, '$1')
-    };
-  });
+/*
+  We could just render tag dynamically for now,but it's easier to customize it (style/behavior) later if each tag has a separate component
+ */
+const H1 = ({ text }) => <h1>{text}</h1>;
+
+const H2 = ({ text }) => <h2>{text}</h2>;
+
+const P = ({ text }) => <p class='markdonwn-p'>{text}</p>;
+
+const Code = ({ text }) => <code class='markdown-code'>{text}</code>;
+
+const TagKind = {
+  H1: 'h1',
+  H2: 'h2',
+  P: 'p',
+  CODE: 'code',
 };
 
-// END OF MD_PARSER
-
-
-// VIEW LOGIC
-
-const MarkdownEditor = ({ md, onChange }) => {
-  const handleChange = event => {
-    onChange(event.target.value);
-  };
-  return <textarea value={md} onChange={handleChange} />;
+const tagToComponentMap = {
+  [TagKind.H1]: H1,
+  [TagKind.H2]: H2,
+  [TagKind.P]: P,
+  [TagKind.CODE]: Code,
 };
 
-const Preview = ({ md }) => {
-  const parsedMd = parseMd(md);
 
+const Preview = ({ content }) => {
   return (
     <div className="App-Preview">
       MD Preview
       <hr />
+      {
+        content.map(({ tag, text }) => {
+          let Component = tagToComponentMap[tag];
+          if (!Component) {
+            console.error(`${tag} is not supported`);
+            Component = P;
+          }
 
-      {parsedMd.map(({ tag, text }) => {
-        return React.createElement(tag, {}, text)
-      })}
+          return <Component text={text} />
+        })
+      }
     </div>
-  );
+  )
+}
+
+/*
+  structure that defines start and end sequences for line to be parsed as corresponding tag
+    [patternStart: string, tagKind: TagKind, patternEnd: string]
+    empty string should be provided if start or end pattern should be empty
+*/
+const TagToMarkdownTypes = [
+  ['## ', TagKind.H2, ''], 
+  ['# ', TagKind.H1, ''],
+  /* not clear if ```foo``` should be transformed, or space before text is required (behavior can be adjusted easily by pattern adjustment)
+    
+    inline code is not supported
+  */
+  ['```', TagKind.CODE, '```'],
+  // always true
+  ['', TagKind.P, ''],
+];
+
+const getTagFromMarkdownString = (str) => {
+  // looks like in most implementation whitespace symbols are not preventing markdown from recognizing the pattern
+  const trimmedStr = str.trimStart();
+  return TagToMarkdownTypes
+    .find(([patternStart, , patternEnd ]) => trimmedStr.startsWith(patternStart) && trimmedStr.endsWith(patternEnd));
 };
 
-const initialMd = "# My name is Mohit.\n\n## My name is actually not Mohit.\n\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.\n\n```undefined < undefined // false```";
+
+const parseLine = (line, index) => {
+  const [patternStart, tag, patternEnd] = getTagFromMarkdownString(line);
+
+  /* 
+    trim start of the string for all tags 
+    except p (want to preserve whitespace for normal text)
+  */
+  const trimmedString = tag === TagKind.P 
+    ? line : line.trimStart();
+  return {
+    line: index + 1,
+    tag,
+    text: trimmedString.substring(
+      patternStart.length, 
+      trimmedString.length - patternEnd.length
+    ),
+  };      
+};
+
+const textRepresentationToMarkdown = (text) => {
+  return text.map(({ tag, text }) => {
+    const type = TagToMarkdownTypes.find(([, t]) => tag === t);
+
+    return `${type[0]}${text}${type[2]}`;
+  // extra new line to separate markdown entries
+  }).join('\n\n'); 
+};
 
 const App = () => {
-  const [markdown, setMarkdown] = React.useState(initialMd);
+  const [textRepresentation, setTextRepresentation] = React.useState([]);
+  const [text, setText] = React.useState('');
+
+  const parseRawText = (e) => {
+    const value = e.currentTarget.value;
+    const lines = value.split('\n');
+
+    const result = lines.filter(line => line.length > 0).map(parseLine);
+
+    setText(value);
+    setTextRepresentation(result);
+  };
+
+  React.useEffect(() => {
+    setText(textRepresentationToMarkdown(res));
+    setTextRepresentation(res);
+  }, []);
 
   return (
     <div className="App">
-      <MarkdownEditor md={markdown} onChange={setMarkdown} />
-      <Preview md={markdown} />
+      <textarea value={text} onChange={parseRawText}/>
+      <Preview content={textRepresentation}/>
     </div>
-  );
-};
+  )
+}
 
 export default App
